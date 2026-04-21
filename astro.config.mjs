fc7b1ff636b254b278c6b7f7348833a0f2f91546
MAZE <remvze@proton.me>
2026-04-21 17:08:44 +0330

    Add essential Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
-  integrations: [react()],
+  site: "https://eample.com",
+  integrations: [react(), sitemap(), robotsTxt()],
   vite: {
     plugins: [tailwindcss()],
   },
